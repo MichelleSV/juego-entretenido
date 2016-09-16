1e7e7ef26256f4c00670272c24e8cdaa4f0f3262
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-16 12:39:34 -0500

    añadiendo cri

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 window.addEventListener("load", function() {
     var cri = document.getElementById("cri");
     cri.addEventListener("click", function() {
-        document.getElementById("c1").style.border = "5px solid red";
+        var contador = 1;
+		if(contador == 1){
+			document.getElementById("color").placeholder = "Azul";
+			document.getElementById("c1").style.border = "5px solid red";
+			document.getElementById("c2").style.border = "0px";
+			document.getElementById("c3").style.border = "0px";
+		}
+		else if(contador == 2){
+			document.getElementById("color").placeholder = "Rojo";
+			document.getElementById("c1").style.border = "0px";
+			document.getElementById("c2").style.border = "5px solid green";
+			document.getElementById("c3").style.border = "0px";
+		}
+		else if(contador == 3){
+			document.getElementById("color").placeholder = "Verde";
+			document.getElementById("c1").style.border = "0px";
+			document.getElementById("c2").style.border = "0px";
+			document.getElementById("c3").style.border = "5px solid red";
+			contador = 0;
+		}
+		contador++;
     });
     var superCri = document.getElementById("superCri");
     superCri.addEventListener("click", function() {
