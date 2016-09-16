5b4132b979a121699153a642f9c82101891ef712
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-16 13:39:33 -0500

    subiendo ej

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 window.addEventListener("load", function() {
     var cri = document.getElementById("cri");
-    cri.addEventListener("click", function() {
-        document.getElementById("c1").style.border = "5px solid red";
+    var contador = 1;
+	cri.addEventListener("click", function(){
+		if(contador == 1){
+			document.getElementById("c1").style.border = "5px solid red";
+			document.getElementById("color").placeholder = "Azul";
+			document.getElementById("c2").style.border = "0px";
+			document.getElementById("c3").style.border = "0px";
+		}
+		else if(contador == 2){
+			document.getElementById("c1").style.border = "0px";
+			document.getElementById("color").placeholder = "Rojo";
+			document.getElementById("c2").style.border = "4px solid green";
+			document.getElementById("c3").style.border = "0px";
+		}
+		else if(contador == 3){
+			document.getElementById("c1").style.border = "0px";
+			document.getElementById("color").placeholder = "Verde";
+			document.getElementById("c2").style.border = "0px";
+			document.getElementById("c3").style.border = "4px solid blue";
+			contador = 0;
+		}
+		contador++;
     });
     var superCri = document.getElementById("superCri");
     superCri.addEventListener("click", function() {
